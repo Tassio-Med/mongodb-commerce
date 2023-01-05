2835082a0793f6783613e3ff6f87ba91ab7225b1
Tassio-Med <tassiohfmed@gmail.como>
2023-01-05 16:39:50 -0300

    :sparkles: Retorna o nome de todos os lanches que possuam calorias abaixo de 500

---
+++ challenges/desafio9.js
@@ -0,0 +1,11 @@
+db.produtos.find(
+  {
+    valoresNutricionais: {
+      $elemMatch: {
+        tipo: "calorias",
+        quantidade: { $lt: 500 },
+      },
+    },
+  },
+  { _id: false, nome: 1 },
+);
